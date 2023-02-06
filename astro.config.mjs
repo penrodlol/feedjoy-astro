7b944d4ed124525973aa777d8758a272b9ab96b2
penrodlol <christian@penrod.contact>
2023-02-05 21:05:52 -0500

    🔍 forgot site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://feedjoy.gg',
   integrations: [
     preact(),
     tailwind(),
