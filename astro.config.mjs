777badbaea777d04f2bc1b33b25944a722b8ab23
penrodlol <christian@penrod.contact>
2023-02-05 21:04:35 -0500

    🔍 sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [
+    preact(),
+    tailwind(),
+    sitemap({ changefreq: 'daily', lastmod: new Date() }),
+    robotsTxt({ host: true, policy: [{ userAgent: '*', disallow: ['/404'] }] }),
+  ],
 });
