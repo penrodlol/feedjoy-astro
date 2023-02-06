6d038debcdc050c326e839d1a013f77c7f4f9598
penrodlol <christian@penrod.contact>
2023-02-05 20:36:12 -0500

    🔧 Redirect home to page 1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
         DEFAULT:
           '0 3px 5px -1px rgba(0,0,0,.2), 0 5px 8px 0 rgba(0,0,0,.14), 0 1px 14px 0 rgba(0,0,0,.12)',
       },
-      animation: { 'fade-in': 'fade-in 0.25s ease-in-out' },
+      animation: { 'fade-in': 'fade-in 0.15s ease-in-out' },
       keyframes: { 'fade-in': { from: { opacity: 0 }, to: { opacity: 1 } } },
     },
   },
