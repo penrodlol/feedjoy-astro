a7d1e1e48be8e05911b534eda25080a1b5c78c7d
penrodlol <christian@penrod.contact>
2023-03-12 14:04:33 -0400

    🐛 didnt fix it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import netlify from '@astrojs/netlify/functions';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
@@ -10,7 +12,7 @@ export default defineConfig({
   integrations: [
     preact(),
     tailwind(),
-    // sitemap({ changefreq: 'daily', lastmod: new Date() }),
-    // robotsTxt({ host: true, policy: [{ userAgent: '*', disallow: ['/404'] }] }),
+    sitemap({ changefreq: 'daily', lastmod: new Date() }),
+    robotsTxt({ host: true, policy: [{ userAgent: '*', disallow: ['/404'] }] }),
   ],
 });
