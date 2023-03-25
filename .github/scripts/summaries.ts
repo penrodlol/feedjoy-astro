21ccc88ad707db8012f1111c83454b4ecebfb5bc
penrodlol <christian@penrod.contact>
2023-03-25 00:59:02 -0400

    ✨ summaries script

---
+++ .github/scripts/summaries.ts
@@ -0,0 +1,36 @@
+import { createClient } from '@supabase/supabase-js';
+import { load } from 'cheerio';
+import { Configuration, OpenAIApi } from 'openai';
+import { parallel, sift } from 'radash';
+import type { Database } from '../../src/lib/supabase/types';
+
+const supabase = createClient<Database>(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE!,
+);
+
+const openai = new OpenAIApi(
+  new Configuration({ apiKey: process.env.OPENAI_API_KEY! }),
+);
+
+const posts = await supabase.from('post').select().is('summary', null);
+if (posts.error) throw posts.error;
+
+const summaries = sift(
+  await parallel(15, posts.data, async (post) => {
+    const $ = load(await fetch(post.link).then((res) => res.text()));
+    const content = $('main p').children().remove().end().text().slice(0, 3800);
+    const payload = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'user', content: `Summarize in 1 paragraph: ${content}` },
+      ],
+    });
+
+    const summary = payload.data.choices?.[0]?.message?.content;
+    return summary != null ? { id: post.id, summary } : null;
+  }),
+);
+
+const payload = await supabase.rpc('bulk_update_summaries', { summaries });
+if (payload.error) throw payload.error;
