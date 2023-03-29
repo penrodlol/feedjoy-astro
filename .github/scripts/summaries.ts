3f283a4969e7b97d699ba5ea5725f86ab33011ff
penrodlol <christian@penrod.contact>
2023-03-28 22:35:07 -0400

    🐛 Fake a user agent string to bypass some sites requiring it (Medium).

--- .github/scripts/summaries.ts
+++ .github/scripts/summaries.ts
@@ -3,6 +3,7 @@ import { load } from 'cheerio';
 import fetch from 'node-fetch';
 import { Configuration, OpenAIApi } from 'openai';
 import { parallel, sift } from 'radash';
+import UserAgent from 'user-agents';
 import type { Database } from '../../src/lib/supabase/types';
 
 const supabase = createClient<Database>(
@@ -17,12 +18,14 @@ const openai = new OpenAIApi(
 const posts = await supabase.from('post').select().is('summary', null);
 if (posts.error) throw posts.error;
 
+const headers = { 'User-Agent': new UserAgent(/Chrome/).toString() };
 const summaries = sift(
-  await parallel(15, posts.data, async (post) => {
-    const $ = load(await fetch(post.link).then((res) => res.text()));
+  await parallel(15, posts.data, async ({ id, link }) => {
+    const $ = load(await fetch(link, { headers }).then((res) => res.text()));
     $('header, footer, aside, noscript').remove();
     const root = $('main').length > 0 ? $('main p') : $('body p');
-    const text = root.text().slice(0, 8000);
+    const text = root.text().split(' ').slice(0, 8000).join(' ');
+
     const payload = await openai.createChatCompletion({
       model: 'gpt-4',
       messages: [
@@ -31,7 +34,7 @@ const summaries = sift(
     });
 
     const summary = payload.data.choices?.[0]?.message?.content;
-    return summary != null ? { id: post.id, summary } : null;
+    return summary != null ? { id, summary } : null;
   }),
 );
 
