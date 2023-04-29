906e04a9b1ce043e00075af26a6a6628246db939
penrodlol <christian@penrod.contact>
2023-04-28 20:58:41 -0400

    ✨ some template search responses

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -10,12 +10,16 @@ export default function Search() {
     <>
       <dialog
         ref={dialog}
-        class="h-[80vh] w-[90vw] max-w-screen-md rounded bg-1 p-0 text-current
-               shadow backdrop:bg-3 backdrop:bg-opacity-75"
+        class="relative h-[80vh] w-[90vw] max-w-screen-sm rounded bg-1 p-0
+               text-current shadow backdrop:bg-3 backdrop:bg-opacity-90"
         onClick={(e) => e.target === dialog.current && dialog.current?.close()}
       >
-        <SearchInput />
-        <SearchOutput />
+        <div class="flex h-full flex-col gap-6 overflow-hidden px-7 py-3">
+          <SearchInput />
+          <div class="flex-1 self-center overflow-y-auto px-2">
+            <SearchOutput />
+          </div>
+        </div>
       </dialog>
       <button
         class="group flex items-center gap-2 rounded bg-3 px-2 py-1 shadow text-xs"
@@ -23,7 +27,7 @@ export default function Search() {
       >
         <SearchIcon class="h-3 w-3 group-hover:stroke-brand" aria-hidden />
         <span class="group-hover:text-brand">search posts</span>
-        <div class="ml-1 flex items-center gap-1 rounded bg-1 px-1">
+        <div class="ml-1 flex items-center gap-1 rounded bg-1 px-2">
           <Command class="h-3 w-3" aria-hidden />
           <kbd class="translate-y-[0.5px]">K</kbd>
         </div>
--- src/components/search/search-output.tsx
+++ src/components/search/search-output.tsx
@@ -1,3 +1,41 @@
+import { useStore } from '@nanostores/preact';
+import { Loader2 } from 'lucide-preact';
+import { store } from './store';
+
 export default function SearchOutput() {
-  return <div></div>;
+  const $store = useStore(store);
+
+  if ($store.pristine) return <Pristine />;
+  else if ($store.loading) return <Loading />;
+  else if (!$store.posts.length) return <NoResults />;
+  else return <div></div>;
+}
+
+function Loading() {
+  return (
+    <div class="mt-24 flex flex-col items-center gap-2 text-center">
+      <Loader2 class="h-10 w-10 motion-safe:animate-spin" aria-hidden />
+      <p class="text-xl">loading posts...</p>
+    </div>
+  );
+}
+
+function NoResults() {
+  return (
+    <div class="mt-24 text-center">
+      <p class="text-xl">no posts found</p>
+      <p class="max-w-[40ch] text-2 text-sm">try adjusting your query</p>
+    </div>
+  );
+}
+
+function Pristine() {
+  return (
+    <div class="mt-24">
+      <p class="text-xl">discover posts accross sites</p>
+      <p class="max-w-[40ch] text-2 text-sm">
+        query a post title or summary in the search bar above to begin
+      </p>
+    </div>
+  );
 }
